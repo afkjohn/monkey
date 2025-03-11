6030ea20c950579ff087eb1adb17616c7f3336a5
afkjohn <benonpoom@gmail.com>
2025-03-11 12:19:25 +0700

    writing token and lexer_test

---
+++ token/token.go
@@ -0,0 +1,28 @@
+package token
+
+type TokenType string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y ...
+	INT   = "INT"   // 134356
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimeters
+	COMMA     = "."
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
